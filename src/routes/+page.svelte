316e6de482750619d9367cf0b135e3ce1e6b7aec
Nico <nicola.zorzo@gmail.com>
2023-09-18 22:21:35 +0200

    update ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
-<h1 class="text-3xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn">Button</button>
+<script lang="ts">
+  const appName: string = 'realmlisto';
+</script>
+
+<!-- ====================================================================== -->
+
+<header class="p-4">
+  <h1 class="text-3xl">{appName}</h1>
+</header>
+
+<div class="flex flex-col flex-grow p-4">
+  <div class="flex flex-row gap-4">
+    <button class="btn btn-primary no-animation">
+      <i class="bi bi-folder"></i>
+    </button>
+
+    <input 
+      type="text" 
+      class="input input-bordered font-mono"
+      placeholder="Realmlist location" 
+      readonly 
+    />
+  </div>
+</div>
