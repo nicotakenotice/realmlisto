a0e5b36c5fc2b77ef6a702e4e814589daa9beff2
Nico <nicola.zorzo@gmail.com>
2023-09-19 23:25:58 +0200

    convert electron files to .cjs, add file dialog

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,30 +1,53 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain, Menu } = require('electron');
 const path = require('node:path');
+const fs = require('fs/promises');
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.cjs')
     }
   });
 
-  app.isPackaged
-    ? mainWindow.loadFile('../src/app.html')
-    : mainWindow.loadURL('http://localhost:5173');
+  if (app.isPackaged) {
+    Menu.setApplicationMenu(null);
+    mainWindow.loadFile('../src/app.html');
+  }
+  else {
+    mainWindow.loadURL('http://localhost:5173');
+  }
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('getRealmlist$', getRealmlist$);
+
   createWindow();
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) 
+    if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
-  })
+    }
+  });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') 
+  if (process.platform !== 'darwin') {
     app.quit();
+  }
 });
+
+/* ========================================================================= */
+
+const getRealmlist$ = async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog();
+  if (!canceled) {
+    const realmlistPath = filePaths[0];
+    if (realmlistPath.endsWith('.wtf')) {
+      const realmlistContent = await fs.readFile(realmlistPath, { encoding: 'utf8' });
+      return { path: realmlistPath, content: realmlistContent.trim() };  
+    }
+  }
+  return { path: '', content: '' };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,49 @@
 <script lang="ts">
+  import { Realmlist } from '$lib/models';
+
   const appName: string = 'realmlisto';
+  let realmlist: Realmlist = new Realmlist();
+
+  const getRealmlist$ = async () => {
+    await window.electronApi.getRealmlist$()
+      .then((value) => {
+        if (value.path) {
+          realmlist = value;
+        }
+      });
+  };
 </script>
 
 <!-- ====================================================================== -->
 
 <header class="p-4">
-  <h1 class="text-3xl">{appName}</h1>
+  <h1 class="text-3xl text-ellipsis">{appName}</h1>
 </header>
 
-<div class="flex flex-col flex-grow p-4">
+<div class="flex flex-col flex-grow gap-4 p-4">
   <div class="flex flex-row gap-4">
-    <button class="btn btn-primary no-animation">
-      <i class="bi bi-folder"></i>
-    </button>
-
-    <input 
+    <input
+      class="input input-bordered font-mono w-full lg:w-1/2"
       type="text" 
-      class="input input-bordered font-mono"
       placeholder="Realmlist location" 
       readonly 
+      bind:value={realmlist.path}
+    />
+
+    <button 
+      class="btn btn-primary no-animation" 
+      on:click={getRealmlist$}
+    >
+      <i class="bi bi-folder"></i>
+    </button>
+  </div>
+
+  <div>
+    <textarea 
+      class="textarea textarea-bordered font-mono resize-none w-full lg:w-1/2" 
+      placeholder="Realmlist content"
+      readonly
+      bind:value={realmlist.content}
     />
   </div>
 </div>
