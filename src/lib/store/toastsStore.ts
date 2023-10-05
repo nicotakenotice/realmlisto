0f4ff2bc5087c2fd1a2357aa2e95c65706d25490
Nico <nicola.zorzo@gmail.com>
2023-10-06 00:01:35 +0200

    limit displayed toasts

--- src/lib/store/toastsStore.ts
+++ src/lib/store/toastsStore.ts
@@ -1,10 +1,15 @@
 import type { Toast } from '$lib/models/Toast';
 import { writable } from 'svelte/store';
 
+const TOASTS_DISPLAYED: number = 3;
+
 export const toasts = writable<Toast[]>([]);
 
 export const addToast = (toast: Toast) => {
-  toasts.update((all) => [toast, ...all]);
+  toasts.update((all) => all.length > TOASTS_DISPLAYED 
+    ? [toast, ...all.slice(0, TOASTS_DISPLAYED)]
+    : [toast, ...all]);
+
   setTimeout(() => dismissToast(toast.id), toast.duration);
 };
 
